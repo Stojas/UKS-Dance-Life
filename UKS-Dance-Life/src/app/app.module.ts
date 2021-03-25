1d1ad76c16b95bcff0ce03a916a099c4662d7bfe
Łukasz Stojko <stojko.lukasz@gmail.com>
2021-03-25 12:21:37 +0100

    Half

--- UKS-Dance-Life/src/app/app.module.ts
+++ UKS-Dance-Life/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './helper/header/header.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
