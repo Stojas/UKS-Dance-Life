fe3aeb3c5e34b0af50ca500fcb933e2b2f4c4b8e
Łukasz Stojko <stojko.lukasz@gmail.com>
2021-08-12 20:26:38 +0200

    Push

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -14,6 +14,7 @@ import { HttpClient } from '@angular/common/http';
 export class ContactComponent implements OnInit {
   
   public form: FormGroup;
+  faEvelope = faEnvelope;
 
   constructor(private stateService: StateService, public sanitizer: DomSanitizer, public offerService: OffersService, public http: HttpClient) { }
 
